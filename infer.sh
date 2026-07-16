940d3ebe4da1d6dffaa61652e52ddd5e5685a545
CR4ZYM4D <madhavdixit482@gmail.com>
2026-07-17 04:04:02 +0530

    even indexes were uploaded just install start redis and run inference and it should work

--- infer.sh
+++ infer.sh
@@ -8,6 +8,6 @@ sudo systemctl start redis-server
 # python3 pipeline/pipeline_dataset.py
 # model already trained uncomment to train new one
 # python3 pipeline/pipeline_train.py
-python3 pipeline/pipeline_index.py
+# python3 pipeline/pipeline_index.py
 
 uvicorn app:app --reload
