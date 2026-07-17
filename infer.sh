36f2e4d695f67101e3183e4d4319ef7f2fbc95be
CR4ZYM4D <madhavdixit482@gmail.com>
2026-07-17 10:45:12 +0530

    possibly final commit pre deployment

--- infer.sh
+++ infer.sh
@@ -5,9 +5,13 @@ pip3 install -r requirements.txt
 
 sudo systemctl start redis-server
 
+# 3200 images downloaded and labeled
 # python3 pipeline/pipeline_dataset.py
+
 # model already trained uncomment to train new one
 # python3 pipeline/pipeline_train.py
+
+# db already indexed no need to re run alone  unless model also retrained
 # python3 pipeline/pipeline_index.py
 
 uvicorn app:app --reload
